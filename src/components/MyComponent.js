33d019d9d9f3c7cec932cf64c780ee038b68f183
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-11 16:23:43 +0700

    Làm quen với state-một biến object của js

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,11 +2,16 @@ import React from "react";
 
 class MyComponent extends React.Component {
 
+    state = {
+        name: 'HaryPhamDev',
+        address: 'Hoi Dan IT',
+        age: 26
+    };
     // JSX
     render() {
         return (
-            <div> my first component 
-                { Math.random() }
+            <div>
+                My name is {this.state.name} and I'm from {this.state.address}
             </div>
         );
     }
