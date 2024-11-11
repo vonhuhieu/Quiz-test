79081e400c3c27b4c46350bb3901b15e21a59e6d
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-11 18:26:22 +0700

    Làm quen với setState và chuyển qua dùng arrow function

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,21 +8,29 @@ class MyComponent extends React.Component {
         age: 26
     };
 
-    handleClick(event){
-        console.log("My name is ", this.state.name);
-        // console.log(event)
+    handleClick(event) {
+        
+        // merge State => react class
+        this.setState({
+            name: 'Eric',
+            age: Math.floor((Math.random() * 100) + 1)
+        })
+
+        // this.setState({
+        //     age: Math.floor((Math.random() * 100) + 1)
+        // })
     }
 
     handleOnMouseOver(event){
-        console.log(event.pageX);
+        // console.log(event.pageX);
     }
     // JSX
     render() {
         return (
             <div>
-                My name is {this.state.name} and I'm from {this.state.address}
+                My name is {this.state.name} and I'm {this.state.age}
                 <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
-                <button onClick={this.handleClick}>Click me</button>
+                <button onClick={(event) => {this.handleClick(event)}}>Click me</button>
             </div>
         );
     }
