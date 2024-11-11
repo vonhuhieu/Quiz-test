8accc2617eeef5b55d221eb39ccbc19703c784fc
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-11 23:32:04 +0700

    Sử dụng câu điều kiện để giúp code ít đi

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,20 +1,40 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+    state = {
+        isShowListUser: true,
+    };
+    handleShowHide = () => {
+        this.setState({
+            isShowListUser: !this.state.isShowListUser,
+        })
+    };
     render() {
         // destructuring array/object
         const { listUsers } = this.props;
+        // console.log(listUsers);
         // props => viết tắt properties
-        return listUsers.map((value, key) => {
-            return (
-                <div key={value.id}>
-                    <div>User's ID: {value.id}</div>
-                    <div>User's Name: {value.name}</div>
-                    <div>User's Age: {value.age}</div>
-                    <br />
+        return (
+            <div>
+                <div>
+                    <span onClick={() => { this.handleShowHide() }}>{this.state.isShowListUser == true ? "hide" : "show"} list users</span>
                 </div>
-            );
-        });
+                {this.state.isShowListUser &&
+                    <div>
+                        {listUsers.map((value, key) => {
+                            return (
+                                <div key={value.id} className={parseInt(value.age) > 18 ? "green" : "red"}>
+                                    <div>User's ID: {value.id}</div>
+                                    <div>User's Name: {value.name}</div>
+                                    <div>User's Age: {value.age}</div>
+                                    <br />
+                                </div>
+                            );
+                        })}
+                    </div>
+                }
+            </div>
+        );
     }
 }
 export default DisplayInfor;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,7 +5,7 @@ import DisplayInfor from "./DisplayInfor";
 class MyComponent extends React.Component {
     state = {
         listUsers:[
-            {id: 1, name: "Hoi Dan IT", age: "30"},
+            {id: 1, name: "Hoi Dan IT", age: "16"},
             {id: 2, name: "Eric", age: "26"},
             {id: 3, name: "HaryPhamDev", age: "69"},
         ]
