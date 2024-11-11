550b6386af0fa0cfed9fd20a432f09433f5fd861
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-11 20:48:22 +0700

    props

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,12 +1,18 @@
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
     // JSX
     render() {
+        const myInfor = ['ab','c','c'];
         return (
             <div>
                 <UserInfor/>
+                <br/><br/>
+                <DisplayInfor name="Hoi Dan IT" age="30"/>
+                <hr/>
+                <DisplayInfor name={"Eric"} age={2} myInfor={myInfor}/>
             </div>
         );
     }
