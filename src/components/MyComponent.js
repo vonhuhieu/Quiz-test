1d4f02d2844f7efee34035f01d1ee259862f3f6e
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-11 17:00:46 +0700

    Đang gặp lỗi khi sử dụng this.state.name

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,11 +7,22 @@ class MyComponent extends React.Component {
         address: 'Hoi Dan IT',
         age: 26
     };
+
+    handleClick(event){
+        console.log("My name is ", this.state.name);
+        // console.log(event)
+    }
+
+    handleOnMouseOver(event){
+        console.log(event.pageX);
+    }
     // JSX
     render() {
         return (
             <div>
                 My name is {this.state.name} and I'm from {this.state.address}
+                <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
+                <button onClick={this.handleClick}>Click me</button>
             </div>
         );
     }
