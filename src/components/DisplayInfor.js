5b4797cb388d2e4ca8eab107be5652c7187a332e
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-13 06:04:11 +0700

    lifeCycle of React

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -4,10 +4,28 @@ import logo from '../logo.svg'
 
 class DisplayInfor extends React.Component {
     constructor(props){
+        console.log('call constructor: 1');
         super(props);
         this.state={
             isShowListUser: true,
         };
+    }
+    componentDidMount(){
+        console.log('call me component did mount');
+        setTimeout(() => {
+            document.title = 'Eric';
+        }, 3000);
+    }
+
+    componentDidUpdate(prevProps,prevState,snapshot){
+        console.log('call me component did update', this.props,prevProps);
+        if(this.props.listUsers != prevProps.listUsers)
+        {
+            if(this.props.listUsers.length === 5)
+            {
+                alert("you got 5 users");
+            }
+        }
     }
     handleShowHide = () => {
         this.setState({
@@ -15,6 +33,7 @@ class DisplayInfor extends React.Component {
         })
     };
     render() {
+        console.log('call me render');
         // destructuring array/object
         const { listUsers } = this.props;
         // console.log(listUsers);
