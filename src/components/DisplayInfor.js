f3ce66cbcd6eab5bdbc4ee6a1d141350f6398529
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-12 13:22:39 +0700

    Làm việc với hình ảnh

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './DisplayInfor.scss';
+import logo from '../logo.svg'
 
 class DisplayInfor extends React.Component {
     state = {
@@ -17,6 +18,7 @@ class DisplayInfor extends React.Component {
         // props => viết tắt properties
         return (
             <div className="display-infor-container">
+                <img src={logo}/>
                 <div>
                     <span onClick={() => { this.handleShowHide() }}>{this.state.isShowListUser == true ? "hide" : "show"} list users</span>
                 </div>
