a52689277ef22bde3364ad1a763410e075338c8b
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-12 08:52:39 +0700

    Truyền function từ cha sang con

--- src/components/AddUserInfor.js
+++ src/components/AddUserInfor.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-class UserInfor extends React.Component {
+class AddUserInfor extends React.Component {
     state = {
-        name: 'HaryPhamDev',
+        name: '',
         address: 'Hoi Dan IT',
-        age: 26
+        age: ''
     };
 
     handleOnChangeInput = (event) => {
@@ -23,7 +23,11 @@ class UserInfor extends React.Component {
 
     handleOnSubmit = (event) => {
         event.preventDefault();
-        console.log(this.state);
+        this.props.handleAddNewUser({
+            id: Math.floor((Math.random()*100) + 1) + 'random',
+            name: this.state.name,
+            age: this.state.age,
+        });
     }
     render() {
         return (
@@ -50,4 +54,4 @@ class UserInfor extends React.Component {
     }
 }
 
-export default UserInfor;
+export default AddUserInfor;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,25 +1,33 @@
 import React from "react";
-import UserInfor from "./UserInfor";
+import AddUserInfor from "./AddUserInfor";
 import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
     state = {
-        listUsers:[
-            {id: 1, name: "Hoi Dan IT", age: "16"},
-            {id: 2, name: "Eric", age: "26"},
-            {id: 3, name: "HaryPhamDev", age: "69"},
+        listUsers: [
+            { id: 1, name: "Hoi Dan IT", age: "16" },
+            { id: 2, name: "Eric", age: "26" },
+            { id: 3, name: "HaryPhamDev", age: "69" },
         ]
+    };
+
+    handleAddNewUser = (userObj) => {
+        this.setState({
+            listUsers:[userObj,...this.state.listUsers]
+        })
     };
     // JSX
     render() {
         // DRY: don't repeat yourself
         return (
             <div>
-                <UserInfor/>
-                <br/><br/>
-                <DisplayInfor listUsers={this.state.listUsers}/>
-                <hr/>
-                
+                <AddUserInfor handleAddNewUser={this.handleAddNewUser}/>
+                <br /><br />
+                <DisplayInfor
+                    listUsers={this.state.listUsers}
+                />
+                <hr />
+
             </div>
         );
     }
