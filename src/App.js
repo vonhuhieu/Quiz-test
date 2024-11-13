b14fb3799777fbc03bc6bdc8e11bb48bfcbb0c69
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-13 21:24:28 +0700

    Cài react router và bootstrap, cài bearer và set up để bắt đầu dự án

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
-import React from 'react';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='app-container'>
-        Hello world Hoi Dan IT
-        <MyComponent></MyComponent>
-      </div>
-    );
-  }
+
+const App = () => {
+  return (
+    <div className="app-container">
+      hello world
+    </div>
+  );
 }
-
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello world Hoi Dan IT
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
 export default App;
