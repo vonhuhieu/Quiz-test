2b88fccfccb0b9e65bd08a3b8108e787c4a08bee
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-14 17:50:29 +0700

    Cài thư viện react-pro-side-bar và react icon, tạo sidebar

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,7 +1,24 @@
+import SideBar from "./SideBar";
+import './Admin.scss';
+import { FaHeart, FaBars } from 'react-icons/fa';
+import { useState } from "react";
+
+
 const Admin = (props) => {
+    const [collapsed, setCollapsed] = useState(false);
+    const handleCollapsed = () => {
+        setCollapsed(!collapsed);
+    };
+    
     return (
-        <div>
-            Admin component
+        <div className="admin-container">
+            <div className="admin-sidebar">
+                <SideBar collapsed={collapsed}/>
+            </div>
+            <div className="admin-content">
+                <FaBars onClick={() => {handleCollapsed()}}/>
+                content go here
+            </div>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -23,9 +23,8 @@ root.render(
         <Route path='/' element={<App />}>
           <Route index element={<HomePage />} />
           <Route path='/users' element={<User />} />
-          <Route path='/admin' element={<Admin />} />
         </Route>
-
+        <Route path='/admin' element={<Admin />} />
       </Routes>
     </BrowserRouter>
     {/* </React.StrictMode> */}
