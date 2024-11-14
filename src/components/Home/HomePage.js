4076e252f6aba0e1efbfd5d317ac3d1e0ac594f4
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-14 09:44:09 +0700

    CSS cho video

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,13 @@
+import VideoHomePage from '../../assets/video-homepage.webm'
 const HomePage = (props) => {
     return (
         <div className="homepage-container">
-            homepage component
+            <video autoPlay muted loop>
+                <source
+                    src={VideoHomePage}
+                    type="video/webm"
+                />
+            </video>
         </div>
     );
 };
